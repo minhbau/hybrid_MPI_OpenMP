a17489b2d782cc579b90da028c5279dfeb190fde
minhbau <minhbau.luong@kaust.edu.sa>
2021-10-28 00:14:32 +0300

    compute Pi using hybrid mpi and openMP ref: https://staff.sharcnet.ca/jemmyhu/tutorials/MPI+OpenMP_2016.pdf

---
+++ hybrid_hello.c
@@ -0,0 +1,28 @@
+// mpicc -Wall -fopenmp hybrid_hello.c -o hybrid_hello
+// export OMP_NUM_THREADS=4
+// mpirun -np 2 ./hybrid_hello
+
+#include <stdio.h>
+#include "mpi.h"
+#include <omp.h>
+
+int main(int argc, char *argv[]) {
+ int numprocs;
+ int rank;
+ int tid = 0;
+ int np = 1;
+
+ MPI_Init(&argc, &argv);
+ MPI_Comm_size(MPI_COMM_WORLD, &numprocs);
+ MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+
+ #pragma omp parallel default(shared) private(tid, np)
+ {
+   np = omp_get_num_threads();
+   tid = omp_get_thread_num();
+   printf("Hello from thread %2d out of %d from process %d\n",
+          tid, np, rank);
+ }
+
+ MPI_Finalize();
+}
